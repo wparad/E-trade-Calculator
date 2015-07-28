0dadcc6cd2fc483bcf73516c17e50ae408323af0
Warren Parad <wparad@gmail.com>
2015-07-27 21:20:27 -0400

    Create rakefile task.

--- rakefile.rb
+++ rakefile.rb
@@ -47,3 +47,16 @@ task :build => [OUTPUT_DIR] do
     %x[git push --quiet https://#{ENV['GIT_TAG_PUSHER']}@#{GIT_REPOSITORY} #{tag} > /dev/null 2>&1]
   end
 end
+
+task :default => [:temp]
+task :temp do
+  if ENV['TRAVIS']
+    #Setup up deploy
+    puts %x[git config --global user.email "builds@travis-ci.com"]
+    puts %x[git config --global user.name "Travis CI"]
+    tag = VERSION
+    puts %x[git tag #{tag} -a -m "Generated tag from TravisCI for build #{ENV['TRAVIS_BUILD_NUMBER']}"]
+    puts "Pushing Git tag #{tag}."
+    %x[git push --quiet https://#{ENV['GIT_TAG_PUSHER']}@#{GIT_REPOSITORY} #{tag} > /dev/null 2>&1]
+  end
+end
