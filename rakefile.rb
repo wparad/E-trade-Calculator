0ffc1609605b8dd0d36eac2a068a2593e9c2cedd
Warren Parad <wparad@gmail.com>
2015-07-29 21:06:56 -0400

    Create C# solution file.

--- rakefile.rb
+++ rakefile.rb
@@ -1,8 +1,10 @@
-#!/bin/
+#!/usr/bin/ruby -e
 require 'rake'
 require 'rake/clean'
 require 'fileutils'
 require 'tmpdir'
+
+#Environment variables: http://docs.travis-ci.com/user/environment-variables/
 
 PWD = File.dirname(__FILE__)
 OUTPUT_DIR = File.join(PWD, 'output')
@@ -10,12 +12,19 @@ SOLUTION_SLN = Dir[File.join(PWD, '*.sln')].first
 GIT_REPOSITORY = %x[git config --get remote.origin.url].split('://')[1]
 match = (ENV['TRAVIS_BRANCH'] || '').match(/^release\/(.*)$/)
 VERSION = "#{match && match[1] ? match[1] : '0.0'}.#{ENV['TRAVIS_BUILD_NUMBER']}"
-#Environment variables: http://docs.travis-ci.com/user/environment-variables/
+
 directory OUTPUT_DIR
+
+desc 'Build the solution.'
 task :build => [OUTPUT_DIR] do
   raise 'Nuget restore failed' if !system("nuget restore #{SOLUTION_SLN}")
   system('nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner')
   raise 'Build failed' if !system("xbuild /p:Configuration=Release #{SOLUTION_SLN}")
+end
+
+task :default => [:build, :create_nuget_package, :create_git_tag]
+
+task :create_nuget_package do
   Dir.mktmpdir do |tmp|
     nuspec = File.join(tmp, 'Microservice.nuspec')
     File.write(nuspec, "<?xml version=\"1.0\" encoding=\"utf-8\"?>
@@ -36,20 +45,9 @@ task :build => [OUTPUT_DIR] do
 ")
     raise 'Nuget packing failed' if !system("nuget pack '#{nuspec}' -BasePath #{PWD} -OutputDirectory #{OUTPUT_DIR} -Verbosity detailed")
   end
-
-  if ENV['TRAVIS']
-    #Setup up deploy
-    puts %x[git config --global user.email "builds@travis-ci.com"]
-    puts %x[git config --global user.name "Travis CI"]
-    tag = VERSION
-    puts %x[git tag #{tag} -a -m "Generated tag from TravisCI for build #{ENV['TRAVIS_BUILD_NUMBER']}"]
-    puts "Pushing Git tag #{tag}."
-    %x[git push --quiet https://#{ENV['GIT_TAG_PUSHER']}@#{GIT_REPOSITORY} #{tag} > /dev/null 2>&1]
-  end
 end
 
-task :default => [:temp]
-task :temp do
+task :create_git_tag do
   if ENV['TRAVIS']
     #Setup up deploy
     puts %x[git config --global user.email "builds@travis-ci.com"]
